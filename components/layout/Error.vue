836b083ff5cd550b88cf2337d6724c5d42451533
lebathanh <lebathanhdnc@gmail.com>
2021-12-02 13:40:01 +0700

    up

---
+++ components/layout/Error.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="body_page">
+    <div class="body_page-header">
+      <h2>This page is not available</h2>
+      <p>Please check your address or go back home page to continue...</p>
+      <v-btn color="primary" @click="$router.push('/')">Go home</v-btn>
+    </div>
+    <img src="@/assets/images/404.gif" alt="" srcset="" />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.body_page {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  &-header {
+    position: fixed;
+    width: 100%;
+    text-align: center;
+    margin-top: 150px;
+    top: 0px;
+    h2 {
+      font-size: 30px;
+      text-transform: uppercase;
+    }
+  }
+  img {
+    margin-top: 150px;
+  }
+}
+</style>
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,11 +1,58 @@
 <template>
-  <v-app>
-    <h1>Erro</h1>
-  </v-app>
+  <v-row class="body">
+    <v-col class="body-error" cols="12">
+      <error-component v-if="error.statusCode === 404" />
+      <h1 v-else>
+        {{ otherError }}
+      </h1>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-export default {}
+import ErrorComponent from '@/components/layout/Error.vue'
+
+export default {
+  components: {
+    ErrorComponent,
+  },
+  layout: 'empty',
+
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      pageNotFound: '404 Not Found',
+      otherError: 'An error occurred',
+    }
+  },
+  head() {
+    const title = this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+    return {
+      title,
+    }
+  },
+}
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+h1 {
+  font-size: 20px;
+}
+.body {
+  height: 100%;
+  width: 100%;
+  position: relative;
+  &-error {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    position: relative;
+  }
+}
+</style>
+scoped>
